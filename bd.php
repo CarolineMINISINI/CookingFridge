2ad8c398931f0bb0336f40b4bee1d42102f675fe
Caroline MINISINI <minisini.Caroline@gmail.com>
2015-06-01 18:42:11 +0200

    Initial Import

--- bd.php
+++ bd.php
@@ -3,7 +3,7 @@
 <?php
 	try 
 	{
-		$bdd=new PDO("mysql:host=mysql12.000webhost.com;dbname=a8046592_projet;charset=utf8", "*****", "******", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$bdd=new PDO("mysql:host=localhost;dbname=projet;charset=utf8", "root", "", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 		$bdd->query('SET NAMES utf8');
 	}
 	catch (Exception $e) {die('Erreur : ' . $e->getMessage());}
